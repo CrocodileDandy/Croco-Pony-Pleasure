00dbaa864942470833eea27e317fc7850a2131d2
Rémi Louf <remilouf@gmail.com>
2013-08-25 00:49:58 +0200

    Can't align data series with pandas... grrrrr!

--- idea.md
+++ idea.md
@@ -10,3 +10,14 @@ Viz
 
 * Time lapse of the station's status (%full)
 * Show on a map stations which are more han half full on average and more than half empty
+
+
+Analysis
+========
+
+* Time correlations for the same stations
+	+ Period?
+	+ Can we eliminate the week-ends and bank holidays?
+
+
++ Correlations at a given time for stations -> gives us different classes?
--- viz/extract_data.py
+++ viz/extract_data.py
@@ -11,9 +11,10 @@ list_files=os.listdir(folder)
 for f in list_files:
     try:
         number = int(f.replace('.csv',''))
-        s=pd.read_csv(folder+f,names=['date','slots','bikes'],parse_dates = {'Timestamp' : ['date']}, index_col = 'Timestamp')
-        s.index=pd.to_datetime(s.index,unit='s').tz_localize('UTC').tz_convert('Europe/Paris')
-        data[number]=s
+        if(number in [19021,19031,19011]): #Lets not import everything when testing
+	        s=pd.read_csv(folder+f,names=['date','slots','bikes'],parse_dates = {'Timestamp' : ['date']}, index_col = 'Timestamp')
+	        s.index=pd.to_datetime(s.index,unit='s').tz_localize('UTC').tz_convert('Europe/Paris')
+	        data[number]=s
     except:
         pass
 
@@ -26,12 +27,23 @@ for n in data:
 		index=n
 		l=len(data[n])
 
+print data[19011].head()
+
+
 data[19031].resample('1Min',fill_method='pad')
-print data[19031].describe()
-
 data[19021].resample('1Min',fill_method='pad')
-print data[19021].describe()
+data[19011].resample('1Min',fill_method='pad')
 
-data[19031]['bikes'].plot()
-data[19021]['bikes'].plot()
+
+# data[19011].align(data[19021])
+
+print data[19011].head()
+print data[19021].head()
+
+
+# pouet['bikes'].plot()
+# data[19031]['bikes'].plot()
+# data[19021]['bikes'].plot()
+# data[19011]['bikes'].plot()
+
 plt.show()
