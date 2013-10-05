134a7e6c02820c284c7a9c750d44dbcfdf68c17d
CrocodileDandy <clement.jacquard@gmail.com>
2013-10-06 00:46:02 +0200

    Added functions cut_days, plot_compared_days, plot_bike_time_evol_by_days to the lib. Also slightly modified the analysis file.

--- velib_analyze.py
+++ velib_analyze.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from matplotlib import pyplot as pl
 import numpy as np, velib_lib as velib, os
+from matplotlib import pyplot as pl
 
-data_path = 'Second data - 1 week/'
+data_path = 'data/Second data - 1 week/'
 paris = velib.json_local_loader('Paris.json')
 station_numbers = [station for station in os.listdir(data_path)]
 station_data = dict()
@@ -11,3 +11,9 @@ for station in station_numbers:
     station_data[station] = np.loadtxt(data_path+station,dtype = 'int',delimiter = ',')
 
 # Do whatever you want
+t,vel = velib.mk_bike_time_evol(station_numbers,station_data)
+days_data = velib.cut_days(t[:],vel[:])
+velib.plot_bike_time_evol(t,vel)
+#velib.plot_compared_days(days_data,'15/8/2013')
+velib.plot_bike_time_evol_by_days(days_data)
+pl.show()
--- velib_lib.py
+++ velib_lib.py
@@ -3,7 +3,7 @@
 Functions for the velib main program
 """
 from matplotlib import pyplot as pl
-import json
+import json, time, numpy as np
 
 def json_local_loader(file_path):
     '''
@@ -33,7 +33,7 @@ def mk_interval_hist(station_numbers,station_data):
 
 def mk_bike_time_evol(station_numbers,station_data):
     '''
-    Make the time evolution graph of available bikes
+    Make the time evolution data of available bikes
     '''
     t_init = max([array.ravel()[0] for array in station_data.values()])
     vel_init = sum([array.ravel()[2] for array in station_data.values()])
@@ -54,10 +54,85 @@ def mk_bike_time_evol(station_numbers,station_data):
             t.append(step[0])
         else:
             vel[-1] = (vel[-1]+step[1])
-    pl.plot(t-t_init,vel)
-    pl.xlim(0,max(t)-t_init)
+    return t,vel
+
+
+def cut_days(t,vel):
+    '''
+    Cut cumulated avaiable bikes data and sort it by days
+    '''
+    t_temp = t[:]
+    vel_temp = vel[:]
+    days_data = {}
+    t_day = []
+    vel_day = []
+    day = time.localtime(t_temp[-1])[2]
+    while t_temp != []:
+        if time.localtime(t_temp[-1])[2] == day:
+            t_day.append(t_temp.pop())
+            vel_day.append(vel_temp.pop())
+        else:
+            days_data[str(time.localtime(t_day[-1])[2])+'/'+str(time.localtime(t_day[-1])[1])+'/'+str(time.localtime(t_day[-1])[0])] = zip(reversed(t_day),reversed(vel_day))
+            t_day = []
+            vel_day = []
+            day = time.localtime(t_temp[-1])[2]
+    days_data[str(time.localtime(t_day[-1])[2])+'/'+str(time.localtime(t_day[-1])[1])+'/'+str(time.localtime(t_day[-1])[0])] = zip(reversed(t_day),reversed(vel_day))
+    return days_data
+
+
+def plot_compared_days(days_data,ref_day):
+    '''
+    Plot available bikes number evolution by days, being compared to a specific day
+    '''
+    pl.figure()
+    for day in days_data.keys():
+        first_event = time.localtime(days_data[day][0][0])
+        first_time = 3600*first_event[3]+60*first_event[4]+first_event[5]
+        days_data[day] = [(ele[0] + first_time - days_data[day][0][0],ele[1]) for ele in days_data[day]]
+    ref_data = days_data[ref_day]
+    for day in [day for day in days_data.keys() if day!=ref_day]:
+        ref = ref_data[:]
+        day2process = days_data[day][:]
+        day_comp = []
+        while ref != [] and day2process != []:
+            if ref[0][0] < day2process[0][0]:
+                temp = ref.pop(0)
+                day_comp.append((day2process[0][0],day2process[0][1]-temp[1]))
+            if ref[0][0] > day2process[0][0]:
+                temp = day2process.pop(0)
+                day_comp.append((ref[0][0],temp[1]-ref[0][1]))
+            if day2process[0][0] == ref[0][0]:
+                temp1 = ref.pop(0)
+                temp2 = day2process.pop(0)
+                day_comp.append((temp1[0],temp2[1]-temp1[1]))
+        pl.plot([ele[0] for ele in day_comp],[ele[1] for ele in day_comp],label = day)
+    pl.legend()
+    pl.grid('on')
+    pl.title('# of available bikes compared to 15/08/2013')
+    pl.xlabel('Time (s)')
+    pl.ylabel('# of available bikes')
+
+
+def plot_bike_time_evol_by_days(days_data):
+    pl.figure()
+    for day in days_data.keys():
+        first_event = time.localtime(days_data[day][0][0])
+        first_time = 3600*first_event[3]+60*first_event[4]+first_event[5]
+        days_data[day] = [(ele[0] + first_time - days_data[day][0][0],ele[1]) for ele in days_data[day]]
+    for day in days_data.keys():
+        pl.plot([ele[0] for ele in days_data[day]],[ele[1] for ele in days_data[day]],label = day)
+    pl.legend()
+    pl.grid('on')
+    pl.title('Time evolution of total number of available bikes by days')
+    pl.xlabel('Time (s)')
+    pl.ylabel('# of available bikes')
+
+
+def plot_bike_time_evol(t,vel):
+    pl.figure()
+    pl.plot(t-t[0],vel)
+    pl.xlim(0,max(t)-t[0])
     pl.grid('on')
     pl.title('Time evolution of total number of available bikes')
     pl.xlabel('Time (s)')
     pl.ylabel('Total number of bikes')
-    pl.show()
